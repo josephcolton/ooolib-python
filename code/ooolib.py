0edd74464ed2cc54967d30d7ad8facade3c006a2
Joseph Colton <josephcolton@gmail.com>
2022-12-21 15:36:42 -0800

    Added some objects to ooolib to get started

--- code/ooolib.py
+++ code/ooolib.py
@@ -6,14 +6,42 @@
 # You can contact me by email at josephcolton@gmail.com                         #
 #################################################################################
 
+# Import python modules
+import os
+
+# Import ooolib-python modules
+import ooolibGlobal
+import ooolibMeta
+
+###################################
+# ooolib-python Calc Spreadsheets #
+###################################
 class Calc:
     """Calc - Open Document Format (ODF) spreadsheets
 
     Base object for creating ODF spreadsheets
     """
-
     def __init__(self):
-        pass
+        self.global_object = ooolibGlobal.Global()
+        self.meta = ooolibMeta.Meta(self.global_object)
 
     def config(self, name, value):
         pass
+
+class Writer:
+    """Writer - Open Document Format (ODF) text documents
+
+    Base object for creating ODF text documents
+    """
+    def __init__(self):
+        self.global_object = ooolibGlobal.Global()
+        self.meta = ooolibMeta.Meta(self.global_object)
+
+    def config(self, name, value):
+        pass
+
+###################
+# Execute to Test #
+###################
+if __name__ == "__main__":
+    calc = Calc()
