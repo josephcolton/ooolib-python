095b699cfc7b49b89ed8a289b9657a703cfd98e5
Joseph Colton <josephcolton@gmail.com>
2022-12-22 03:11:37 -0800

    Updated ooolib Calc class to create a blank document

--- code/ooolib.py
+++ code/ooolib.py
@@ -11,7 +11,12 @@ import os
 
 # Import ooolib-python modules
 import ooolibGlobal
+import ooolibFile
 import ooolibMeta
+import ooolibCalcContent
+import ooolibCalcManifest
+import ooolibCalcSettings
+import ooolibCalcStyles
 
 ###################################
 # ooolib-python Calc Spreadsheets #
@@ -23,10 +28,24 @@ class Calc:
     """
     def __init__(self):
         self.global_object = ooolibGlobal.Global()
+        self.manifest = ooolibCalcManifest.Manifest(self.global_object)
         self.meta = ooolibMeta.Meta(self.global_object)
+        self.content = ooolibCalcContent.Content(self.global_object)
+        self.settings = ooolibCalcSettings.Settings(self.global_object)
+        self.styles = ooolibCalcStyles.Styles(self.global_object)
 
     def config(self, name, value):
         pass
+
+    def export(self, filename):
+        f = ooolibFile.SaveFile(filename)
+        f.insertFileString("mimetype", "application/vnd.oasis.opendocument.spreadsheet")
+        f.insertFileString("META-INF/manifest.xml", self.manifest.toString(indent=True))
+        f.insertFileString("meta.xml", self.meta.toString())
+        f.insertFileString("content.xml", self.content.toString())
+        f.insertFileString("settings.xml", self.settings.toString())
+        f.insertFileString("styles.xml", self.styles.toString())
+        f.close()
 
 class Writer:
     """Writer - Open Document Format (ODF) text documents
@@ -44,4 +63,8 @@ class Writer:
 # Execute to Test #
 ###################
 if __name__ == "__main__":
+    # Make sure we can make simple documents
+    os.makedirs("example", exist_ok=True)
+    # Generate an empty calc
     calc = Calc()
+    calc.export("example/simple-calc.ods")
