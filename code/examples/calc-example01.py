9864f497b872e16cb8bf8827aeb421a57be61a8e
Joseph Colton <josephcolton@gmail.com>
2022-12-28 14:37:29 -0800

    Created basic user calc examples

---
+++ code/examples/calc-example01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('..')
+import ooolib
+
+# Create your document
+doc = ooolib.Calc()
+
+# We use the doc object to work with our Calc document.  We need to
+# then address different parts of the document.  For spreadsheets,
+# we typically want to address the current/active table.  We address
+# the activeTable in the content area of the document using:
+#
+# doc.content.activeTable
+#
+# Setting float values.  We index into the table using a row and column.
+# The row and column values both start at 1.  To set float values we
+# use the method setCellFloat:
+#
+# setCellFloat(row, col, value)
+#
+for row in range(1, 9):
+	for col in range(1, 9):
+                value = row*col
+                doc.content.activeTable.setCellFloat(row, col, value)
+
+# Export/save the document to the file you want to create
+doc.export("calc-example01.ods")
---
+++ code/examples/calc-example02.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('..')
+import ooolib
+
+
+# Create the document
+# When you create the document, it is assumed that you want to start
+# with a blank table.  This is automatically down with the autoInit.
+# you can choose to instead make a blank document without any sheets
+# using:
+#
+# autoInit=False
+#
+doc = ooolib.Calc(autoInit=False)
+
+# Since we do not have any sheets, we need to create them before we
+# have an activeTable.
+# We can create a new table (sheet) with the addTable method.
+
+doc.content.addTable("First") # First sheet
+doc.content.activeTable.setCellText(1, 1, "First Sheet")
+
+# We can create a second sheet using the addTable method again.  The
+# activeTable is automatically updated to the newest sheet/table that
+# you create.
+
+doc.content.addTable("Second") # Second sheet
+doc.content.activeTable.setCellText(1, 1, "Second Sheet")
+
+# Let's go ahead and create another one.  This time we will remember
+# where it is using a table object.
+
+thirdSheet = doc.content.addTable("Third") # Third sheet
+thirdSheet.setCellText(1, 1, "Third Sheet")
+
+# The activeTable is once again updated.  If we want to move back,
+# we can use the setActiveTable method to find it.
+
+# Move back to the first sheet
+doc.content.setActiveTable("Second")
+doc.content.activeTable.setCellText(2, 1, "More text on the second sheet")
+
+# You can move the the last sheet by not using a name in the lookup.
+doc.content.setActiveTable() # Should take us to the last sheet
+doc.content.activeTable.setCellText(2, 1, "Text on the last sheet")
+
+# You can also rename a sheet.  You first need to find the sheet,
+# then you can rename it.
+
+doc.content.setActiveTable("First")
+doc.content.activeTable.setTableName("Number 1")
+
+# Write out the document
+doc.export("calc-example02.ods")
