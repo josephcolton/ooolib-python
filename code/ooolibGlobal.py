bd93a2d7d6175150ec55bbeb07722a89b5826d71
Joseph Colton <josephcolton@gmail.com>
2022-12-29 00:17:36 -0800

    Added strings and objects to global object

--- code/ooolibGlobal.py
+++ code/ooolibGlobal.py
@@ -13,28 +13,33 @@ class Global:
     def __init__(self):
         self.project = "ooolib-python"
         self.version = "0.2.0"
-        self.global_ints = {}
+        self.globalInts = {}
+        self.globalStrings = {}
+        self.globalObjects = {}
 
     def getVersion(self):
         versionStr = "%s %s" % (self.project, self.version)
         return versionStr
 
+    ###################
+    # Global Integers #
+    ###################
     def getGlobalInt(self, name, setDefault=0):
         #print("getGlobalInt(%s)" % (name))
         # Sanitize
         setDefault = int(setDefault)
         # Simple get if existing
-        if name in self.global_ints:
-            return self.global_ints[name]
+        if name in self.globalInts:
+            return self.globalInts[name]
         # Missing case
         else:
-            self.global_ints[name] = setDefault
+            self.globalInts[name] = setDefault
             return setDefault
 
     def setGlobalInt(self, name, value):
         #print("setGlobalInt(%s, %s)" % (name, value))
         value = int(value)             # Sanitize value
-        self.global_ints[name] = value # Set value
+        self.globalInts[name] = value # Set value
         return value                   # Return set value
 
     def incrementGlobalInt(self, name, incValue=1):
@@ -42,3 +47,29 @@ class Global:
         value = self.getGlobalInt(name, incValue) # Get current value
         self.setGlobalInt(name, value + incValue) # Increment saved value
         return value                              # Return old value
+
+    ##################
+    # Global Strings #
+    ##################
+    def getGlobalString(self, name, default=""):
+        if name in self.globalStrings:
+            return self.globalStrings[name]
+        # No value
+        return default
+
+    def setGlobalString(self, name, value):
+        self.globalStrings[name] = value
+
+    ##################
+    # Global Objects #
+    ##################
+    def getGlobalObjects(self, name):
+        # Return object if found
+        if name in self.globalObjects:
+            return self.globalObjects[name]
+        # No value
+        print("Missing Object: %s" % name)
+        return None
+
+    def setGlobalObjects(self, name, value):
+        self.globalObjects[name] = value
